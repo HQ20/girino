6b62d5526734567063822087b57c15a04b50c9c3
Bernardo Vieira <bernardo5@live.com.pt>
2019-11-29 12:50:04 +0000

    remove dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-module.exports = function chaiGirino(chai, utils) {
-    const { Assertion } = chai;
-    Assertion.addMethod('responseText', async function (expectedText) {
-        //
-    });
-};
-//# sourceMappingURL=index.js.map
